6f726699e7e172aaa7fcd5fffed325f3487fe4f0
pepe1973 <bodrogipr@gmail.com>
2020-01-27 10:01:21 +0100

    Második kommitálás

--- NTimes/NTimes/Program.cs
+++ NTimes/NTimes/Program.cs
@@ -10,6 +10,37 @@ namespace NTimes
     {
         static void Main(string[] args)
         {
+            Console.Write("Kérem a sztringet: ");
+            string bemeno = Console.ReadLine();
+            Console.Write("Kérem az ismétlés számát: ");
+            int hanyszor = Int32.Parse(Console.ReadLine());
+            Szoveg szoveg = new Szoveg(bemeno);
+            Console.WriteLine($"Az ismételt sztring: {Ismetel(bemeno, hanyszor)}");
+            Console.WriteLine($"Az ismételt sztring: {szoveg.Ismetel(hanyszor)}");
+
+            Console.ReadKey(true);
+        }
+
+        static string Ismetel(string bemeno, int hanyszor)
+        {
+            string kimeno = "";
+
+            if (bemeno.Length == 0)
+            {
+                return kimeno = "Üres sztringgel nem tudok dolgozni!";
+            }
+            else
+            {
+                for (int i = 0; i < bemeno.Length; i++)
+                {
+                    for (int j = 0; j < hanyszor; j++)
+                    {
+                        kimeno = String.Concat(kimeno, bemeno[i]);
+                    }
+                }
+
+                return kimeno;
+            }
         }
     }
 }
---
+++ NTimes/NTimes/Szoveg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTimes
+{
+    class Szoveg
+    {
+        public string Bemeno { get; set; }
+
+        public Szoveg(string bemeno)
+        {
+            this.Bemeno = bemeno;
+        }
+
+        public string Ismetel(int hanyszor)
+        {
+            string kimeno = "";
+
+            if (this.Bemeno.Length == 0)
+            {
+                return kimeno = "Üres sztringgel nem tudok dolgozni!";
+            }
+            else
+            {
+                for (int i = 0; i < this.Bemeno.Length; i++)
+                {
+                    for (int j = 0; j < hanyszor; j++)
+                    {
+                        kimeno = String.Concat(kimeno, this.Bemeno[i]);
+                    }
+                }
+
+                return kimeno;
+            }
+        }
+    }
+}
