f2a6e4501a8be00bf3713cec71193c17f5b47208
pepe1973 <bodrogipr@gmail.com>
2020-01-27 11:09:38 +0100

    Harmadik kommitálás

---
+++ PhoneFormat/PhoneFormat/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] tomb = new int[10];
+
+            //Telefonszam tel = new Telefonszam(tomb); Itt is műkódik!!
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(10);
+            }
+
+            Telefonszam tel = new Telefonszam(tomb);
+
+            Console.WriteLine($"A véletlen telefonszám: {Telefonszam(tomb)}");
+            Console.WriteLine($"A véletlen telefonszám: {tel.TelefonszamSzamito()}");
+
+            Console.ReadKey(true);
+        }
+
+        static string Telefonszam(int[] tomb)
+        {
+            string kimeno = "";
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                kimeno = String.Concat(kimeno, tomb[i]);
+            }
+
+            kimeno = String.Concat("(", kimeno.Substring(0, 3), ") ", kimeno.Substring(3, 3), "-", kimeno.Substring(6));
+
+            return kimeno;
+        }
+    }
+}
---
+++ PhoneFormat/PhoneFormat/Telefonszam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneFormat
+{
+    class Telefonszam
+    {
+        public int[] Telefon { get; set; }
+
+        public Telefonszam(int[] telefon)
+        {
+            this.Telefon = telefon;
+        }
+
+        public string TelefonszamSzamito()
+        {
+            string kimeno = "";
+
+            for (int i = 0; i < this.Telefon.Length; i++)
+            {
+                kimeno = String.Concat(kimeno, this.Telefon[i]);
+            }
+
+            kimeno = String.Concat("(", kimeno.Substring(0, 3), ") ", kimeno.Substring(3, 3), "-", kimeno.Substring(6));
+
+            return kimeno;
+        }
+    }
+}
